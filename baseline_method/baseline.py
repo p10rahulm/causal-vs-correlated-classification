c6452151b76f91b0b03712d2320d59b2f6427566
Rahul Madhavan <rahul.maddy@gmail.com>
2024-08-29 23:03:58 +0530

    baseline model change

--- baseline_method/baseline.py
+++ baseline_method/baseline.py
@@ -26,6 +26,7 @@ import matplotlib
 
 matplotlib.style.use('ggplot')
 
+from model_code.binary_head import MultiHeadBinaryModel
 
 class BinaryDataset(Dataset):
     def __init__(self, x, y):
@@ -51,31 +52,6 @@ class BinaryDataset(Dataset):
 
         return ret_dict
 
-
-class MultiHeadBinaryModel(nn.Module):
-    def __init__(self, input_dim=393216, hidden_dim_1=1000, hidden_dim_2=10, l=37):
-        super(MultiHeadBinaryModel, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim_1 = hidden_dim_1
-        self.hidden_dim_2 = hidden_dim_2
-        self.out_dim = 1
-        self.fc1 = nn.Linear(self.input_dim, self.hidden_dim_1)
-        self.fc2 = nn.Linear(self.hidden_dim_1, self.hidden_dim_2)
-        self.l = l
-
-        for i in range(1, self.l + 1):
-            setattr(self, f"out{i}", nn.Linear(self.hidden_dim_2, self.out_dim))
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-
-        out_list = []
-        for i in range(1, self.l + 1):
-            var = getattr(self, f"out{i}")
-            out_list.append(F.sigmoid(var(x)))
-
-        return [out for out in out_list]
 
 
 def binary_loss_fn(outputs, targets):
---
+++ model_code/binary_head.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+class MultiHeadBinaryModel(nn.Module):
+    def __init__(self, input_dim=393216, hidden_dim_1=1000, hidden_dim_2=10, l=37):
+        super(MultiHeadBinaryModel, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim_1 = hidden_dim_1
+        self.hidden_dim_2 = hidden_dim_2
+        self.out_dim = 1
+        self.fc1 = nn.Linear(self.input_dim, self.hidden_dim_1)
+        self.fc2 = nn.Linear(self.hidden_dim_1, self.hidden_dim_2)
+        self.l = l
+
+        for i in range(1, self.l + 1):
+            setattr(self, f"out{i}", nn.Linear(self.hidden_dim_2, self.out_dim))
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        out_list = []
+        for i in range(1, self.l + 1):
+            var = getattr(self, f"out{i}")
+            out_list.append(F.sigmoid(var(x)))
+
+        return [out for out in out_list]
